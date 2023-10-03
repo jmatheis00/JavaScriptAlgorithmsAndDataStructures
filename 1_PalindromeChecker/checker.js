8633dcacb8ef487ccf5da96187094675dfa338d1
jmatheis00 <jmatheis@student.42heilbronn.de>
2023-10-04 00:50:56 +0200

    PalindromeChecker done

--- 1_PalindromeChecker/checker.js
+++ 1_PalindromeChecker/checker.js
@@ -1,1 +1,13 @@
-replace it when done
+//  string.replace('characterToReplace', '');
+//  /[^a-zA-Z0-9]/g searches any character that is not between the brackets
+function palindrome(str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, '');
+    let reverse_str = "";
+    for(let i = str.length - 1; i >= 0; i--)
+        reverse_str += str[i];
+    return str.toUpperCase() == reverse_str.toUpperCase();
+}
+
+//TESTS
+console.log(palindrome("eye_."));
+console.log(palindrome("eye1"));
