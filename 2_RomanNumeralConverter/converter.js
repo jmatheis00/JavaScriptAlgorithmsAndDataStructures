b05f04d03c75d97002c8b5f96a37c6d1a4f83e19
jmatheis00 <jmatheis@student.42heilbronn.de>
2023-10-08 13:59:20 +0200

    RomanNumeralConverter done

--- 2_RomanNumeralConverter/converter.js
+++ 2_RomanNumeralConverter/converter.js
@@ -1,1 +1,17 @@
-replace it when done
+function convertToRoman(num) {
+    let roman ="";
+    let romanNums = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    let normalNums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    
+      for(let i = 0; i < 13; i++)
+      {
+        while(num >= normalNums[i])
+        {
+          num -= normalNums[i];
+          roman += romanNums[i];
+        }
+      }
+   return roman;
+}
+
+console.log(convertToRoman(36));
