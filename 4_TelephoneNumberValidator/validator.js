4ac737c9b26907128d7fbd4975e57eadefa9601e
jmatheis00 <jmatheis@student.42heilbronn.de>
2023-10-09 16:57:31 +0200

    TelephoneNumberValidator

--- 4_TelephoneNumberValidator/validator.js
+++ 4_TelephoneNumberValidator/validator.js
@@ -1,1 +1,16 @@
-replace it when done
+function telephoneCheck(str) {
+    const regex10 = /[(]?\d{3}[)-\s]?\d{3}[-\s]?\d{4}$/;
+    const regex11 = /^1\s?\(?\d{3}[)-]?\s?\d{3}[-\s]?\d{4}$/;
+    let countNum = str.match(/[\d]/g).length;
+  
+    if(((/[^\d-()" "]/g).test(str) || countNum < 10 || countNum > 11)
+          || (str.indexOf('(') < 0 && str.indexOf(')') >= 0)
+          || (str.indexOf('(') >= 0 && str.indexOf(')') < 0)
+          || (countNum === 10 && regex10.test(str) == false)
+          || (countNum === 11 && regex11.test(str) == false))
+      return false;
+    return true;
+}
+
+// TESTS
+console.log(telephoneCheck("555-555-5555"));
